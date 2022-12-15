025391abfc34217d575e6c96f4b2201c8e4f1151
Bryan Smith <smitbry17@gmail.com>
2022-12-14 22:59:34 -0800

    finalized majority of code and tested in insomnia

--- Develop/Routes/api.js
+++ Develop/Routes/api.js
@@ -1,24 +1,26 @@
-const route = require('express').Router;
-const store = require('../db/store');
+const route = require("express").Router();
+const store = require("../db/store");
 
-route.get('notes', (req, res) => {
-    store.getNotes()
+route.get("/notes", (req, res) => {
+  store
+    .getNotes()
     .then((notes) => {
-        return res.json(notes);
+      return res.json(notes);
     })
     .catch((err) => {
-        res.status(400).json(err);
-    })
-})
+      res.status(400).json(err);
+    });
+});
 
-route.post('notes', (req,res) => {
-    store.addNote(req.body)
+route.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
     .then((note) => {
-        res.json(note);
+      res.json(note);
     })
     .catch((err) => {
-        res.status(400).json(err);
-    })
-})
+      res.status(400).json(err);
+    });
+});
 
-module.exports() = route;
+module.exports = route;
