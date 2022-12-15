c2576baa20f737e530ce2a8583106967a864b35f
Bryan Smith <smitbry17@gmail.com>
2022-12-14 20:27:03 -0800

    added necessary files and finalized route files

---
+++ Develop/Routes/api.js
@@ -0,0 +1,24 @@
+const route = require('express').Router;
+const store = require('../db/store');
+
+route.get('notes', (req, res) => {
+    store.getNotes()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
+})
+
+route.post('notes', (req,res) => {
+    store.addNote(req.body)
+    .then((note) => {
+        res.json(note);
+    })
+    .catch((err) => {
+        res.status(400).json(err);
+    })
+})
+
+module.exports() = route;
