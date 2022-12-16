c128a3adc5ce195d732847284d059b66af4e5a3b
Bryan Smith <smitbry17@gmail.com>
2022-12-16 02:44:25 -0800

    trying to fix routing on heroku server

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.static("public"));
 app.use("/api", route);
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Note-Tracker/public/notes.html"));
+  res.sendFile(path.join(__dirname, "../../Note-Tracker/public/notes.html"));
 });
 
 // All other routes respond with the index.html file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Note-Tracker/public/index.html"));
+  res.sendFile(path.join(__dirname, "../../Note-Tracker/public/index.html"));
 });
 
 app.listen(PORT, () => {
